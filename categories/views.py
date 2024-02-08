3ad48a816647e0c26f8cadca7e36fa14277e105e
Akbermet <akbermet.zh@gmail.com>
2024-02-08 15:47:33 +0100

    category: create endpoint works

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from categories.models import Category
+from categories.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryListCreateView(APIView):
+    def get(self, request):
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        return Response("post method!")
--- task_repo/settings.py
+++ task_repo/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
+    "rest_framework",
     "crispy_forms",
     "crispy_bootstrap5",
     "tasks",
