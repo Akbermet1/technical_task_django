f5b0d51cf8e95f6cf786ea23368493d8ae5d59f4
Akbermet <akbermet.zh@gmail.com>
2024-02-08 18:45:06 +0100

    ListCreateTaskView implemented post, added authentication and permissions

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class ListCreateTaskView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         all_tasks = Task.objects.all()
         serializer = TaskSerializer(all_tasks, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
