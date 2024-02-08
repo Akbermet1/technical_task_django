b696b1430bf99a85a80869eeac299c4bea4c2f61
Akbermet <akbermet.zh@gmail.com>
2024-02-08 02:25:46 +0100

    implemented category, task models and registered in admin

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from categories.models import Category
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=70)
+    completed = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="tasks")
+
+    def __str__(self):
+        return self.title
