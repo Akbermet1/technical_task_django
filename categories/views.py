3014d14465a09c6b49ca6fc41d51817ee449bfa9
Akbermet <akbermet.zh@gmail.com>
2024-02-08 15:55:12 +0100

    category : post method implemented

--- categories/views.py
+++ categories/views.py
@@ -12,4 +12,8 @@ class CategoryListCreateView(APIView):
         return Response(serializer.data)
     
     def post(self, request):
-        return Response("post method!")
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
