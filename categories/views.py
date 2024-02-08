57cf078c6ee7bc0142666ac596401c0bd3c71f07
Akbermet <akbermet.zh@gmail.com>
2024-02-08 17:10:25 +0100

    CategoryDetailView : methods put, delete implemented

--- categories/views.py
+++ categories/views.py
@@ -31,4 +31,17 @@ class CategoryDetailView(APIView):
         category = self.get_object(pk)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category, data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
